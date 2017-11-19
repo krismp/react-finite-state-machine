a2d2799f5e8aec545e264b4ffa9b8b2d18990954
Kris Mulyandani Putra <kris.mp7777@gmail.com>
2017-11-19 19:44:07 +0700

    Implement finite state machine with xstate

--- src/App.js
+++ src/App.js
@@ -1,33 +1,100 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Machine } from 'xstate';
+
+const ROOT_URL = 'https://api.github.com/users';
+const myMachine = Machine({
+  initial: 'idle',
+  states: {
+    idle: {
+      on: {
+        CLICK: 'loading'
+      }
+    },
+    loading: {
+      on: {
+        RESOLVE: 'data',
+        REJECT: 'error'
+      }
+    },
+    data: {
+      on: {
+        CLICK: 'loading'
+      }
+    },
+    error: {
+      on: {
+        CLICK: 'loading'
+      }
+    }
+  }
+});
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      label: null
+
+  state = {
+    data: {},
+    dataState: 'idle',
+    input: ''
+  }
+
+  searchRepositories = async () => {
+    try {
+      const data = await fetch(`${ROOT_URL}/${this.state.input}`).then(response => response.json());
+      this.setState(({ data }), this.transition('RESOLVE'));
+    } catch (error) {
+      this.transition('REJECT');
     }
   }
 
-  handleOnChange = e => {
+  commands = {
+    loading: this.searchRepositories
+  }
+
+  transition = action => {
+    const { dataState } = this.state;
+
+    const newState = myMachine.transition(dataState, action).value;
+    const command = this.commands[newState];
+
     this.setState({
-      label: e.target.value
-    })
+      dataState: newState
+    }, command);
   }
 
   render() {
-    const { label } = this.state;
+    const { data, dataState } = this.state;
+    const buttonText = {
+      idle: 'Fetch Github',
+      loading: 'Loading ...',
+      error: 'Github fail. Retry?',
+      data: 'Fetch again?'
+    }[dataState];
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React, {label}</h1>
-        </header>
-        <p className="App-intro">
-          <input type="text" onChange={this.handleOnChange} />
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            <input
+              type="text"
+              value={this.state.input}
+              onChange={e => this.setState({ input: e.target.value })}
+            />
+            <button
+              onClick={() => this.transition('CLICK')}
+              disabled={dataState === 'loading'}
+            >
+              {buttonText}
+            </button>
+          </p>
+        </div>
+        {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+        {dataState === 'error' && <h1>Error!!!</h1>}
       </div>
     );
   }
